fca977d14ff44b195694bd7d9401e989455b684a
AC <alvernchen@gmail.com>
2019-04-12 18:00:04 -0400

    add name tooltip feature
    maybe a toggle later

---
+++ allnames.js
@@ -0,0 +1,7 @@
+// stores names for every character in every game ...
+// tooltip functionality from http://jsfiddle.net/2vc8y5ox/ and
+// https://stackoverflow.com/questions/15702867/html-tooltip-position-relative-to-mouse-pointer !
+const allCharacters = [	[],
+						["Mars", "Luke"],
+						["Mars", "Sheeda", "Jeigan"],
+						["Marth", "Kris"],];
