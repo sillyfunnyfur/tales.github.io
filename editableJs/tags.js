c0dd6b937066fb1b234feee3bf4281ed176d6b70
sillyfunnyfur <tricksterdistri@gmail.com>
2024-06-12 17:34:39 -0700

    Update tags.js

--- editableJs/tags.js
+++ editableJs/tags.js
@@ -11,24 +11,8 @@ const thief = "thief"
 allTags = [femaleCharacter, maleCharacter, dancer, apothecary, warrior, hunter, scholar, merchant, cleric, thief];
 
 const imgTags = {
-    "octo1":{
-        1: [femaleCharacter, cleric],
-        2: [maleCharacter, scholar],
-        3: [femaleCharacter, merchant],
-        4: [maleCharacter, warrior],
-        5: [femaleCharacter, dancer],
-        6: [maleCharacter, apothecary],
-        7: [maleCharacter, thief],
-        8: [femaleCharacter, hunter]
+    "
     },
-    "octo2": {
-        1: [femaleCharacter, hunter],
-        2: [femaleCharacter, apothecary],
-        3: [femaleCharacter, thief],
-        4: [maleCharacter, scholar],
-        5: [maleCharacter, merchant],
-        6: [femaleCharacter, dancer],
-        7: [maleCharacter, cleric],
-        8: [maleCharacter, warrior]
+    
     },
 }
